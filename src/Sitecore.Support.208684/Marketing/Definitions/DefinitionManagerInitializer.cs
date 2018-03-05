cdd95b898ffe168556a6e86de51f32a59934b6fc
DK\mao <mao@sitecore.net>
2018-03-05 18:39:19 +0200

    Create singleton instance during initialization to avoid deadlocks on aggregation start.

---
+++ src/Sitecore.Support.208684/Marketing/Definitions/DefinitionManagerInitializer.cs
@@ -0,0 +1,21 @@
+﻿using Sitecore.DependencyInjection;
+using Sitecore.Marketing.Definitions;
+using Sitecore.Marketing.Definitions.Outcomes.Model;
+using Sitecore.Pipelines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Marketing.Definitions
+{
+  public class DefinitionManagerInitializer
+  {
+    public void Process(PipelineArgs args)
+    {
+      // Sitecore.Marketing.Definitions.Outcomes.Data.ItemDb.ItemOutcomeDefinitionRepository is a singleton.
+      // Creating its instance during initialization helps to ensure no deadlocks occur when aggregation starts for the first time.
+      var outcomeDefinition = ServiceLocator.ServiceProvider.GetService(typeof(IDefinitionManager<IOutcomeDefinition>));
+    }
+  }
+}
